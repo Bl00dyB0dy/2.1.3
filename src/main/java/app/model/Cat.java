411302fecd13a0335aaee45f8bb7ef342ccac1b4
Bl00dyB0dy <stepnova_marina@bk.ru>
2024-04-23 23:04:36 +0300

    first commit

---
+++ src/main/java/app/model/Cat.java
@@ -0,0 +1,21 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat extends Animal {
+
+    private final String saying;
+
+    public Cat(String saying) {
+        this.saying = saying;
+    }
+    @Override
+    void say() {
+        System.out.println(saying);
+    }
+    @Override
+    public String toString() {
+        return "Im a Cat";
+    }
+}
---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,20 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal{
+    private final String saying;
+
+    public Dog(String saying) {
+        this.saying = saying;
+    }
+    @Override
+    void say() {
+        System.out.println(saying);
+    }
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+}
